450bc94ab3fb1bf6ecf458a53a1d2c825f10e247
Alumno <Alumno@PC-INFOR00>
2019-02-19 12:23:40 +0100

    filta

---
+++ src/com/seeds/web/filter/InitFilter.java
@@ -0,0 +1,55 @@
+package com.seeds.web.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+@WebFilter("/*")
+public class InitFilter implements Filter {
+	
+	private static Logger logger = LogManager.getLogger(InitFilter.class);
+
+    public InitFilter() {
+    	
+    }
+
+	public void destroy() {
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+		HttpServletResponse httpResponse = (HttpServletResponse) response;
+		
+		String paxinaRafa = "/html/rafa.jsp";
+		String ipRafa = "10.53.124.205";
+		
+		if(httpRequest.getRemoteAddr().equals(ipRafa) && !httpRequest.getServletPath().contains(paxinaRafa)) {
+			
+			logger.debug("Entrou ip: "+ httpRequest.getRemoteAddr());
+			httpResponse.sendRedirect(httpRequest.getContextPath()+"/html/rafa.jsp");
+			
+		} else {
+			
+			logger.debug("Entrou ip: "+ httpRequest.getRemoteAddr());
+			chain.doFilter(httpRequest, httpResponse);
+		
+		}
+	}
+
+	public void init(FilterConfig fConfig) throws ServletException {
+	}
+
+}
