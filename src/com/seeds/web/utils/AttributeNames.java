7fbad11e8444afea58d081560754008e5d69cd55
Alumno <Alumno@PC-INFOR00>
2019-02-20 13:23:29 +0100

    deletes

--- src/com/seeds/web/utils/AttributeNames.java
+++
@@ -1,7 +0,0 @@
-package com.seeds.web.utils;
-
-public interface AttributeNames {
-
-	public static final String ERRORS = "errors";
-	public static final String RESULTADOS = "resultados";
-}
--- src/com/seeds/web/utils/ControllerPath.java
+++
@@ -1,8 +0,0 @@
-package com.seeds.web.utils;
-
-public class ControllerPath {
-	
-	public static final String USUARIO = "/SeeDsWebTraining/usuario";
-
-
-}
